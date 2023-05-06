f2692fdffa2086d5cef63436fa78cd6372e55888
hunkim@gmail.com <hunkim@gmail.com>
2023-05-06 15:08:14 +0900

    Update error message

--- gpt_util.py
+++ gpt_util.py
@@ -1,24 +1,32 @@
 import os
 
 import openai
+import logging
 
 CHATGPT_ENGINE = os.getenv("CHATGPT_ENGINE", "gpt-3.5-turbo")
-BOT_PENCIL_ICON = os.getenv("BOT_PENCIL_ICON", "*")
+BOT_PENCIL_ICON = os.getenv("BOT_PENCIL_ICON", "✍")
 
-UPDATE_CHAR_RATE = 7
+UPDATE_CHAR_RATE = 5
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 
 async def chatgpt_callback_response(messages, call_back_func, call_back_args):
+    content: Literal[''] = ""
+
     try:
+
         response = openai.ChatCompletion.create(
             model=CHATGPT_ENGINE,
             messages=messages,
             stream=True,
         )
-
-        content = ""
 
         # Stream each message in the response to the user in the same thread
         counter = 0
@@ -28,7 +36,7 @@ async def chatgpt_callback_response(messages, call_back_func, call_back_args):
                 content += completions.choices[0].delta.get("content")
 
             if call_back_func and call_back_args:
-                if counter % UPDATE_CHAR_RATE == 1:
+                if counter % UPDATE_CHAR_RATE == 0:
                     # Send or update the message,
                     # depending on whether it's the first or subsequent messages
                     call_back_args['text'] = content+BOT_PENCIL_ICON
@@ -39,4 +47,7 @@ async def chatgpt_callback_response(messages, call_back_func, call_back_args):
     except (KeyError, IndexError) as e:
         return "GPT3 Error: " + str(e)
     except Exception as e:
+        logger.error("GPT3 Unknown Error: " + str(e))
+        if content:
+            return content
         return "GPT3 Unknown Error: " + str(e)
